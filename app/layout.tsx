9f7cf70ccc6ced7c37107c6717b057c0af9555fe
hridoyhaque01 <hridoyhaque.info@gmail.com>
2026-06-24 22:39:51 +0600

    feat: initialize project structure with tailwind configuration, root layout, and base components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,20 @@
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist, Inter } from "next/font/google";
 
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+import Footer from "@/components/shared/Footer";
+import Navbar from "@/components/shared/Navbar";
+import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
+import "./globals.css";
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'})
 
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
+
+const inter = Inter(
+  {
+    subsets: ["latin"],
+    variable: "--font-inter",
+  }
+)
 
 export default function RootLayout({
   children,
@@ -20,10 +25,16 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
+      className={cn("antialiased", "font-inter", geist.variable,
+          inter.variable
+      )}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <Navbar />  
+          {children}
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   )
